1ea90d4658a05e0d8a8cb7e36ab6829f24894936
r17x <ri7nz@evilfactory.id>
2023-06-25 01:33:01 +0700

    refactor: seperated templates

---
+++ templates.nix
@@ -0,0 +1,22 @@
+{
+  go = {
+    path = ./go;
+    description = "Go development environment";
+  };
+
+  node = {
+    path = ./node;
+    description = "Nodejs development environment";
+  };
+
+  node14 = {
+    path = ./node14;
+    description = "Nodejs (v14) and pnpm (v5) development environment";
+  };
+
+  react-native = {
+    path = ./react-native;
+    description = "React Native development environment";
+  };
+
+}
